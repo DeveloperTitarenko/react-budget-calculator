d341780021db135af58b398d07ce71666002a761
DeveloperTitarenko <DeveloperTitarenko@gmail.com>
2020-08-18 01:44:47 +0300

    block history, add method addDescription and addAmount

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const Header = ()=> {
+    return(
+        <header>
+            <h1>Кошелек</h1>
+            <h2>Калькулятор расходов</h2>
+        </header>
+    )
+}
+export default Header;
